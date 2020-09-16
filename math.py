aa903e055c482b78985595afd1078073be478168
NPGCoding <70853437+NPGCoding@users.noreply.github.com>
2020-09-16 18:27:54 +0530

    Add files via upload
    file addef

---
+++ math.py
@@ -0,0 +1,10 @@
+# read two numbers
+number1 = int(input("Enter the 1st number :"))
+number2 = int(input("Enter the 2nd number :"))
+# choose the smaller number
+if number1 < number2 :
+    smaller_number = number1
+else:
+    larger_number = number2
+# print the result
+print("The smaller number is :", smaller_number)
